4467283d534bf82e7def9003682ecddbd173d92f
Dorian Masquelier <dorian.masquelier@gmail.com>
2023-02-20 11:42:14 +0100

    delete index column

--- create_db.sql
+++ create_db.sql
@@ -1,10 +1,8 @@
 CREATE DATABASE 'trello-clone-back';
 CREATE TABLE IF NOT EXISTS tasks(
+    id SERIAL PRIMARY KEY,
     state character varying(50) NOT NULL CHECK(state in 
-    ('todo', 'inprogress', 'done', 'blocked') ),
-    id integer NOT NULL,
+    ('todo', 'inprogress', 'done', 'blocked')),
     title character varying(100) NOT NULL,
-    content text,
-    index integer,
-    CONSTRAINT tasks_pkey PRIMARY KEY (id)
+    content text NOT NULL,
 );
